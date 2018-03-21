5eff6f5d1e0a24245224c026aa924c62d8491937
RenBrian <Brian_Ren@outlook.com>
2018-03-21 17:23:39 +0800

    "springboot starter"

---
+++ oldneighborhood-statistics/src/main/java/com/oldneighborhood/demo/OldneighborhoodStatisticsApplication.java
@@ -0,0 +1,12 @@
+package com.oldneighborhood.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldneighborhoodStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldneighborhoodStatisticsApplication.class, args);
+	}
+}
